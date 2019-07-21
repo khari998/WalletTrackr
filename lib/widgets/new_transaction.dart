2554bf769121331387ee3ac1a13dae95f2097100
Khari <khari.lane@khalikenterprises.com>
2019-07-21 09:38:27 -0500

    Added number keyboard to amount TextField, Input submits to list on keyboard submit, Numbers are rounded to nearest hundredth decimal place

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -4,9 +4,21 @@ class NewTransaction extends StatelessWidget {
   final Function addTx;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+  void submitInput() {
+    final submittedTitle = titleController.text;
+    final submittedAmount = double.parse(amountController.text);
+
+    if (submittedTitle.isEmpty || submittedAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      submittedTitle,
+      submittedAmount,
+    );
+  }
 
   NewTransaction(this.addTx);
-
 
   @override
   Widget build(BuildContext context) {
@@ -18,17 +30,17 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitInput(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => submitInput(),
             ),
             FlatButton(
               child: Text('Add Transaction'),
-              onPressed: () => addTx(
-                titleController.text,
-                double.parse(amountController.text),
-              ),
+              onPressed: submitInput,
               textColor: Colors.purple,
             )
           ],
