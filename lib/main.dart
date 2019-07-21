834d6ab66effc9de93ec098c64e8082152b8360e
Khari <khari.lane@khalikenterprises.com>
2019-07-20 22:06:27 -0500

    Added Flutter controllers to TextFields in order to capture user input

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,9 @@ class MyHomePage extends StatelessWidget {
     )
   ];
 
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,9 +59,11 @@ class MyHomePage extends StatelessWidget {
                 children: <Widget>[
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
                   ),
                   FlatButton(
                     child: Text('Add Transaction'),
