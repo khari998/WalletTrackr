f93305ca6b39e73aff01f8c0aaacb8b0fb16f260
Khari <khari.lane@khalikenterprises.com>
2019-07-21 17:03:59 -0500

    Test commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WalletTrackr',
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.redAccent,
