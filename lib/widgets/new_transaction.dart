fd4f425a9fe7fd47c8785a5bdcfddc9dc978d4de
Khari <khari.lane@khalikenterprises.com>
2019-07-21 12:52:52 -0500

    Added pseudocode

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
+  NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  // grabs text from input fields
   final titleController = TextEditingController();
   final amountController = TextEditingController();
-  void submitInput() {
-    final submittedTitle = titleController.text;
-    final submittedAmount = double.parse(amountController.text);
 
+  void submitInput() {
+    final submittedTitle = titleController.text; // store input title string 
+    final submittedAmount = double.parse(amountController.text); // store and covert amount string to double
+
+    // will not allow data to submit under these two circumstances
     if (submittedTitle.isEmpty || submittedAmount <= 0) {
       return;
     }
 
-    addTx(
+    widget.addTx(
       submittedTitle,
       submittedAmount,
     );
-  }
 
-  NewTransaction(this.addTx);
+    // closes top most screen displayed so modal closes after submission
+    Navigator.of(context).pop(); 
+
+  }
 
   @override
   Widget build(BuildContext context) {
