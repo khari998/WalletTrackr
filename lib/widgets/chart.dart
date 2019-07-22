a09fefb654f5f6e077bb07b1846433f2ad1af9c9
Khari <khari.lane@khalikenterprises.com>
2019-07-21 20:00:08 -0500

    Added chart widget. Added logic for pulling most recent date information

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekday = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double dailySum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekday.day &&
+            recentTransactions[i].date.month == weekday.month &&
+            recentTransactions[i].date.year == weekday.year) {
+          dailySum += recentTransactions[i].amount;
+        }
+      }
+      
+
+      return {
+        'day': DateFormat.E().format(weekday),
+        'amount': dailySum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -35,9 +35,11 @@ class _NewTransactionState extends State<NewTransaction> {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 5,
       child: Container(
         padding: EdgeInsets.all(10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
